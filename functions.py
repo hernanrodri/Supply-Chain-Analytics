c51780f19589567b894929f60c274aecbb6b8660
Hernán <hernanrodri459@gmail.com>
2024-08-12 18:06:01 -0300

    Proyecto Enviado

--- functions.py
+++ functions.py
@@ -3,7 +3,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import operator
 
 def combined_table(X, Y, x, y):
     x = x.reshape(1, -1)
@@ -12,18 +11,6 @@ def combined_table(X, Y, x, y):
     y = np.repeat(y, x.shape[1], axis = 1)
     df = pd.DataFrame({X: np.ndarray.flatten(x), Y: np.ndarray.flatten(y)})
     return df
-
-def constraint(df, operators, variables):
-    table = {"<=": operator.le, ">=": operator.ge, "==": operator.eq, "sum": np.sum, "mean": np.mean, "min": np.min, "max": np.max} #lookup table
-    operation1 = table[operators[0]]
-    operation2 = table[operators[1]]
-    operation3 = table[operators[2]]
-    v = variables[-2]
-    w = variables[-1]
-    func = lambda x: {v: operation1(x, axis = 0), w: operation3(x, axis = 0)}[x.name]
-    g = df.groupby(by = variables[:-2]).transform(func)
-    g[v] = np.where(operation2(g[v], g[w]), g[v], g[w])
-    return g
 
 def plot_scatter(X, Y, x, y):
     plt.style.use("seaborn-darkgrid")
